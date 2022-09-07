fe65185acf5d78ae28c3d609148264593a8320ed
Arthur H G Bonora <arthur@bonora.com.br>
2022-09-07 18:28:37 -0300

    0.0.2

--- lib.php
+++ lib.php
@@ -2,7 +2,7 @@
 function htlADDLEAD ($uid_lista, $email, $nome, $sobrenome, $tag) {
   $curl = curl_init();
   curl_setopt_array($curl, array(
-    CURLOPT_URL => $endpoint'.subscribers?api_token='.$token.'&list_uid='.$uid_lista.'&EMAIL='.$email.'&FIRST_NAME='.$nome.'&LAST_NAME='.$sobrenome.'&tag='.$tag,
+    CURLOPT_URL => $endpoint.'/subscribers?api_token='.$token.'&list_uid='.$uid_lista.'&EMAIL='.$email.'&FIRST_NAME='.$nome.'&LAST_NAME='.$sobrenome.'&tag='.$tag,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
